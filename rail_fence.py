31ce50131eaafdb890f0f6a676bbcf81b3d62ed3
bee966561 <bee966561@gmail.com>
2019-11-11 16:13:13 -0800

    made it more elegant and functional:)

--- rail_fence.py
+++ rail_fence.py
@@ -1,17 +1,26 @@
 #This file encrypts text using rail fence transposition.
 
-text	=	'The quick brown fox jumped over the lazy dog and killed it'
-l	=	len(text)
-print(f"\nLength of plain text is {l}\n")
+text	=	input('Enter Plain Text for Rail_Fence_Encryption:\n')
 encText	=	[]
 y	=	''
+fences	=	int(input('Enter number of fences: '))
+print(fences)
+k	=	fences
+l	=	len(text)
+print(l)
 index	=	0
-a	=	0
-while	a!=2:
-	encText.append(text[index])
-	index+=2
-	if index>=l-1:
-		index=1
-		a	+=	1
+encText.append(text[0])
+while	fences>=0 and fences<l-1:
+	index	=	fences
+	print('out')
+	print(index)
+	while	index<=l-1 and index!=0:
+#		print(index)
+		encText.append(text[index])
+#		print(encText)
+		index=index+k
+#		print(index)
+	fences	=fences-1
 
-print(f"The plain text \n{text}\n is encrypted as \n{y.join(encText)}")
+print(f"\nLength of plain text is {l}\n")
+print(f"The plain text \n{text}\n is encrypted as: \n{y.join(encText)}")

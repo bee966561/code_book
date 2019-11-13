b67deef7bd90d9a0e5e4d92e9f327b0a52d77761
bee966561 <bee966561@gmail.com>
2019-11-12 17:52:15 -0800

    a few cool new functions to our generic module

--- generic.py
+++ generic.py
@@ -8,3 +8,22 @@ def fill_list(array, character=1, length=10):
 		if	len(array)	==	length:
 			break
 
+
+def ascii_over_flow(theLetter, theShift):
+	theTotal	=	ord(theLetter)	+	theShift
+	if	theTotal	>	122:
+		return	96	+	theTotal	-	122
+	elif	theTotal	<	97:
+		return	122	-	(96	-	theTotal)
+	else:
+		return	theTotal
+
+
+def enc(passedString, passedShift=10):
+	"""pass lower_case strings to encrypt using shift cipher and returns a list"""
+	newString[]
+	i	=	0
+	for letter in passedString:
+		newString[i]	=	chr(ascii_over_flow(passedString[i], passedShift))
+		i	=	i	+	1
+	return newString
---
+++ shift_encrypt.py
@@ -0,0 +1,6 @@
+#This program encrypts data using caesar shift cipher. bear in mind that this is a weak encryption algorithm
+
+plainText	=	input('Enter plain text: ')
+shift	=	int(input('Enter shift'))
+
+
